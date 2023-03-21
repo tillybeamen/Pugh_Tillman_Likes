02c515fc97e4be39c6452f2c22019300e4f02786
tillybeamen <till.pugh@gmail.com>
2023-03-21 15:42:27 -0700

    fixed buttons

---
+++ script.js
@@ -0,0 +1,27 @@
+var count = 9;
+var countElement1 = document.querySelector("#count-1")
+console.log(countElement1)
+function add1() {
+    count++;
+    countElement1.innerText = count + " likes(s)"
+    console.log(count);
+}
+
+var count = 12;
+var countElement2 = document.querySelector("#count-2")
+console.log(countElement2)
+function add2() {
+    count++;
+    countElement2.innerText = count + " likes(s)"
+    console.log(count);
+}
+
+var count = 9;
+var countElement3 = document.querySelector("#count-3")
+console.log(countElement3)
+function add3() {
+    count++;
+    countElement3.innerText = count + " like(s)"
+    console.log(count);
+}
+
